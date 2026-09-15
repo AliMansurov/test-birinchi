285069a37811e06dfb7e526712067fae09a6ee9c
Shoxrux Abdivaxobov <abduvaxobov1707@gmail.com>
2026-09-15 10:08:35 +0500

    Update main.py with addition and subtraction calculator

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    first = float(input("Enter the first number: "))
+    second = float(input("Enter the second number: "))
+
+    print(f"Addition: {add(first, second)}")
+    print(f"Subtraction: {subtract(first, second)}")
